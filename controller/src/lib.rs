c079834c754f8f4898e8e532813f189bff84df99
Kevin Del Castillo <quebin31@gmail.com>
2020-05-25 10:39:52 -0500

    Make User and Item ids generic

--- controller/src/lib.rs
+++ controller/src/lib.rs
@@ -1,17 +1,21 @@
 use anyhow::Error;
 use std::collections::HashMap;
 
-pub trait User {
-    fn id(&self) -> u64;
+pub trait User<I: Item> {
+    type Id;
+
+    fn id(&self) -> Self::Id;
     fn name(&self) -> &str;
-    fn ratings(&self) -> &HashMap<u64, f64>;
+    fn ratings(&self) -> &HashMap<I::Id, f64>;
     fn metadata(&self) -> HashMap<String, String> {
         HashMap::new()
     }
 }
 
 pub trait Item {
-    fn id(&self) -> u64;
+    type Id;
+
+    fn id(&self) -> Self::Id;
     fn name(&self) -> &str;
     fn metadata(&self) -> HashMap<String, String> {
         HashMap::new()
@@ -20,22 +24,26 @@ pub trait Item {
 
 pub trait Controller<U, I>
 where
-    U: User,
     I: Item,
+    U: User<I>,
 {
     fn with_url(url: &str) -> Self;
-    fn user_by_id(&self, id: u64) -> Result<U, Error>;
-    fn item_by_id(&self, id: u64) -> Result<I, Error>;
+    fn user_by_id(&self, id: U::Id) -> Result<U, Error>;
+    fn item_by_id(&self, id: I::Id) -> Result<I, Error>;
     fn user_by_name(&self, name: &str) -> Result<Vec<U>, Error>;
     fn item_by_name(&self, name: &str) -> Result<Vec<I>, Error>;
     fn all_users(&self) -> Result<Vec<U>, Error>;
+    fn all_users_except(&self, id: U::Id) -> Result<Vec<U>, Error>;
 }
 
-#[derive(Debug, thiserror::Error)]
-pub enum ErrorKind {
-    #[error("Entity with id({0}) not found")]
-    NotFound(u64),
+pub mod error {
+    use std::fmt::{Debug, Display};
 
+    #[derive(Debug, thiserror::Error)]
+    #[error("Entity with id({0}) not found")]
+    pub struct NotFoundById<I: Debug + Display>(pub I);
+
+    #[derive(Debug, thiserror::Error)]
     #[error("Entity with name({0}) not found")]
-    NotFoundByName(String),
+    pub struct NotFoundByName<I: Debug + Display>(pub I);
 }
