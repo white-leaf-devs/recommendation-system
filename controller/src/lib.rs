02032b2cc24d46c6ab0dc6bc09b3a68c4e10fa71
Kevin Del Castillo <quebin31@gmail.com>
2020-05-26 11:36:39 -0500

    Use an Id structure for ids and remove new and with_url from controller

--- controller/src/lib.rs
+++ controller/src/lib.rs
@@ -1,30 +1,48 @@
 use anyhow::Error;
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    hash::{BuildHasher, Hash, Hasher},
+    ops::Deref,
+};
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+pub struct Id(pub String);
+
+impl<T: ToString> From<T> for Id {
+    fn from(t: T) -> Self {
+        Id(t.to_string())
+    }
+}
+
+impl Deref for Id {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
-pub type Ratings<K> = HashMap<K, f64>;
-pub type MapedRatings<U, K> = HashMap<U, Ratings<K>>;
+pub type Ratings = HashMap<u64, f64>;
+pub type MapedRatings = HashMap<Id, Ratings>;
+
+pub fn make_hash<H: BuildHasher, K: Hash>(hasher: &H, key: K) -> u64 {
+    let mut state = hasher.build_hasher();
+    key.hash(&mut state);
+    state.finish()
+}
 
 pub trait Entity {
-    type Id;
-
-    fn get_id(&self) -> Self::Id;
+    fn get_id(&self) -> Id;
     fn get_data(&self) -> HashMap<String, String> {
         Default::default()
     }
 }
 
-pub trait Controller<U, I>
-where
-    U: Entity,
-    I: Entity,
-{
-    fn new() -> Self;
-    fn with_url(url: &str) -> Self;
-    fn user_by_id(&self, id: U::Id) -> Result<U>;
-    fn item_by_id(&self, id: I::Id) -> Result<I>;
-    fn ratings_by_user(&self, user: &U) -> Result<Ratings<I::Id>>;
-    fn ratings_except_for(&self, user: &U) -> Result<MapedRatings<U::Id, I::Id>>;
+pub trait Controller<U, I> {
+    fn user_by_id(&self, id: &Id) -> Result<U>;
+    fn item_by_id(&self, id: &Id) -> Result<I>;
+    fn ratings_by_user(&self, user: &U) -> Result<Ratings>;
+    fn ratings_except_for(&self, user: &U) -> Result<MapedRatings>;
 
     fn user_by_name(&self, name: &str) -> Result<Vec<U>> {
         Err(error::NotFoundByName(name.into()).into())
@@ -36,11 +54,7 @@ where
 }
 
 pub mod error {
-    use std::fmt::{Debug, Display};
-
-    #[derive(Debug, thiserror::Error)]
-    #[error("Entity with id({0}) not found")]
-    pub struct NotFoundById<I: Debug + Display>(pub I);
+    use std::fmt::Debug;
 
     #[derive(Debug, thiserror::Error)]
     #[error("Entity with name({0}) not found")]
