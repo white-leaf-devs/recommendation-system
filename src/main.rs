3d44aff76b077539c479ec2918fc3bcd4c6a660c
Kevin Del Castillo <quebin31@gmail.com>
2020-05-26 20:48:42 -0500

    Implement some commands

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,54 @@
+pub mod parser;
+
 use anyhow::Error;
 use books::BooksController;
 use controller::{Controller, Entity};
+use movie_lens_small::MovieLensSmallController;
+use parser::{Database, Index, Statement};
 use recommend::Engine;
 use simple_movie::SimpleMovieController;
-use text_io::read;
 
 macro_rules! prompt {
     () => {{
-        use std::io::{self, Write};
-
-        print!("{}", PROMPT);
-        io::stdout().lock().flush()
+        let mut rl = rustyline::Editor::<()>::new();
+        rl.readline(&format!("{}", PROMPT))
     }};
 
     ($db:expr) => {{
-        use std::io::{self, Write};
-
-        print!("({}) {}", $db, PROMPT);
-        io::stdout().lock().flush()
+        let mut rl = rustyline::Editor::<()>::new();
+        rl.readline(&format!("({}) {}", $db, PROMPT))
     }};
 }
 
-pub enum Databases {
-    Books,
-    SimpleMovie,
-    MovieLensSmall,
+fn get_user<C, U, I>(controller: &C, index: &Index) -> Option<U>
+where
+    C: Controller<U, I>,
+    U: Entity,
+    I: Entity,
+{
+    match index {
+        Index::Id(id) => match controller.user_by_id(&id) {
+            Ok(user) => Some(user),
+            Err(e) => {
+                println!("{}", e);
+                None
+            }
+        },
+        Index::Name(name) => match controller.user_by_name(&name) {
+            Ok(users) => {
+                if let Some(user) = users.into_iter().next() {
+                    Some(user)
+                } else {
+                    println!("Failed to find by name, empty");
+                    None
+                }
+            }
+            Err(e) => {
+                println!("{}", e);
+                None
+            }
+        },
+    }
 }
 
 fn database_connected_prompt<C, U, I>(controller: C, name: &str) -> Result<(), Error>
@@ -33,11 +57,10 @@ where
     U: Entity,
     I: Entity,
 {
-    let _engine = Engine::with_controller(controller);
+    let engine = Engine::with_controller(&controller);
 
     loop {
-        prompt!(name)?;
-        let opt: String = read!("{}\n");
+        let opt: String = prompt!(name)?;
 
         match opt.trim() {
             "q" | "quit" => {
@@ -50,7 +73,87 @@ where
                 break;
             }
 
-            _compound => continue,
+            line => match parser::parse_line(line) {
+                Some(stmt) => match stmt {
+                    Statement::Connect(_) => println!("Invalid in this context!"),
+
+                    Statement::QueryUser(index) => match index {
+                        Index::Id(id) => match controller.user_by_id(&id) {
+                            Ok(user) => {
+                                println!("User with {:?}", user.get_id());
+                                for (key, val) in user.get_data() {
+                                    println!("- {}: {}", key, val);
+                                }
+                            }
+                            Err(e) => println!("{}", e),
+                        },
+                        Index::Name(name) => match controller.user_by_name(&name) {
+                            Ok(users) => {
+                                if users.is_empty() {
+                                    println!("Empty");
+                                    continue;
+                                }
+
+                                for user in users {
+                                    println!("User with {:?}", user.get_id());
+                                    for (key, val) in user.get_data() {
+                                        println!("- {}: {}", key, val);
+                                    }
+                                }
+                            }
+                            Err(e) => println!("{}", e),
+                        },
+                    },
+
+                    Statement::QueryItem(index) => match index {
+                        Index::Id(id) => match controller.item_by_id(&id) {
+                            Ok(item) => {
+                                println!("Item with {:?}", item.get_id());
+                                for (key, val) in item.get_data() {
+                                    println!("- {}: {}", key, val);
+                                }
+                            }
+                            Err(e) => println!("{}", e),
+                        },
+                        Index::Name(name) => match controller.item_by_name(&name) {
+                            Ok(items) => {
+                                if items.is_empty() {
+                                    println!("Empty");
+                                    continue;
+                                }
+
+                                for item in items {
+                                    println!("Item with {:?}", item.get_id());
+                                    for (key, val) in item.get_data() {
+                                        println!("- {}: {}", key, val);
+                                    }
+                                }
+                            }
+                            Err(e) => println!("{}", e),
+                        },
+                    },
+
+                    Statement::Distance(index_a, index_b, method) => {
+                        let user_a = get_user(&controller, &index_a);
+                        let user_b = get_user(&controller, &index_b);
+
+                        let dist = match (user_a, user_b) {
+                            (Some(user_a), Some(user_b)) => {
+                                engine.distance(&user_a, &user_b, method)
+                            }
+                            (_, _) => None,
+                        };
+
+                        match dist {
+                            Some(dist) => println!("Distance is {}", dist),
+                            None => println!("Impossible to calculate distance"),
+                        }
+                    }
+
+                    Statement::KNN(_, _, _) => {}
+                },
+                None => println!("Invalid syntax"),
+            },
         }
     }
 
@@ -62,11 +165,9 @@ const PROMPT: &str = ">> ";
 
 fn main() -> Result<(), Error> {
     println!("Welcome to recommendation-system {}", VERSION);
-    println!();
 
     loop {
-        prompt!()?;
-        let opt: String = read!("{}\n");
+        let opt: String = prompt!()?;
 
         match opt.trim() {
             "?" | "h" | "help" => {
@@ -81,32 +182,30 @@ fn main() -> Result<(), Error> {
                 break;
             }
 
-            compound => {
-                let splitted: Vec<_> = compound.split(' ').collect();
-                match splitted[0] {
-                    "c" | "connect" => {
-                        println!("Connecting to database {}", &splitted[1]);
-                        match splitted[1] {
-                            "books" => database_connected_prompt(BooksController::new()?, "books")?,
-                            "simple-movie" => database_connected_prompt(
+            empty if empty.is_empty() => {}
+
+            line => match parser::parse_line(line) {
+                Some(stmt) => {
+                    if let Statement::Connect(db) = stmt {
+                        match db {
+                            Database::Books => {
+                                database_connected_prompt(BooksController::new()?, "books")?
+                            }
+                            Database::SimpleMovie => database_connected_prompt(
                                 SimpleMovieController::new()?,
                                 "simple-movie",
                             )?,
-                            unknown => {
-                                println!("Unknown database {}", unknown);
-                                continue;
-                            }
+                            Database::MovieLensSmall => database_connected_prompt(
+                                MovieLensSmallController::new()?,
+                                "movie-lens-small",
+                            )?,
                         }
-                    }
-
-                    wtf if wtf.is_empty() => println!(),
-
-                    wtf => {
-                        println!("Unknown command {}", wtf);
-                        continue;
+                    } else {
+                        println!("Invalid statement in this context!");
                     }
                 }
-            }
+                None => println!("Invalid syntax!"),
+            },
         }
     }
 
