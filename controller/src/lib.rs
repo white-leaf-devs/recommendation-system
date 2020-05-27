bc07e6d5f26ba1487a5853e87f49fd8d751692b2
Kevin Del Castillo <quebin31@gmail.com>
2020-05-27 16:03:03 -0500

    Completely rewritten Controller trait. Implementors can search now using custom columns

--- controller/src/lib.rs
+++ controller/src/lib.rs
@@ -1,47 +1,43 @@
 use anyhow::Error;
-use std::{collections::HashMap, hash::Hash, ops::Deref};
-
 use prettytable::{cell, format::consts::FORMAT_BOX_CHARS, row, table, Table};
+use std::collections::HashMap;
 
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
-pub struct Id(pub String);
-
-impl<T: ToString> From<T> for Id {
-    fn from(t: T) -> Self {
-        Id(t.to_string())
-    }
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum SearchBy {
+    Id(String),
+    Name(String),
+    Custom(String, String),
 }
 
-impl Deref for Id {
-    type Target = str;
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-pub type Result<T> = std::result::Result<T, Error>;
-pub type Ratings = HashMap<Id, f64>;
-pub type MapedRatings = HashMap<Id, Ratings>;
-
-pub fn ratings_to_table(map: &Ratings) -> Table {
-    let mut table = Table::new();
-    for (key, val) in map {
-        table.add_row(row![key.0, val]);
+impl SearchBy {
+    pub fn id(id: &str) -> Self {
+        Self::Id(id.into())
     }
 
-    table.set_format(*FORMAT_BOX_CHARS);
-    table
+    pub fn name(name: &str) -> Self {
+        Self::Name(name.into())
+    }
+
+    pub fn custom(key: &str, val: &str) -> Self {
+        Self::Custom(key.into(), val.into())
+    }
 }
 
 pub trait Entity {
-    fn get_id(&self) -> Id;
-
+    fn get_id(&self) -> String;
     fn get_data(&self) -> HashMap<String, String> {
         Default::default()
     }
+}
 
+pub trait ToTable {
+    fn to_table(&self) -> Table;
+}
+
+impl<E: Entity> ToTable for E {
     fn to_table(&self) -> Table {
-        let mut table = table![["id", self.get_id().0]];
+        let mut table = table![["id", self.get_id()]];
+
         for (key, val) in self.get_data() {
             table.add_row(row![key, val]);
         }
@@ -51,25 +47,64 @@ pub trait Entity {
     }
 }
 
+impl<K, V, B> ToTable for HashMap<K, V, B>
+where
+    K: ToString,
+    V: ToString,
+{
+    fn to_table(&self) -> Table {
+        let mut table = Table::new();
+
+        for (key, val) in self {
+            table.add_row(row![key, val]);
+        }
+
+        table.set_format(*FORMAT_BOX_CHARS);
+        table
+    }
+}
+
+//impl<K, V, B> ToTable for &'_ HashMap<K, V, B>
+//where
+//K: ToString,
+//V: ToString,
+//{
+//fn to_table(&self) -> Table {
+//let mut table = Table::new();
+
+//for (key, val) in *self {
+//table.add_row(row![key, val]);
+//}
+
+//table.set_format(*FORMAT_BOX_CHARS);
+//table
+//}
+//}
+
+pub type Result<T> = std::result::Result<T, Error>;
+pub type Ratings = HashMap<String, f64>;
+pub type MapedRatings = HashMap<String, Ratings>;
+
 pub trait Controller<U, I> {
-    fn user_by_id(&self, id: &Id) -> Result<U>;
-    fn item_by_id(&self, id: &Id) -> Result<I>;
-    fn ratings_by_user(&self, user: &U) -> Result<Ratings>;
-    fn ratings_except_for(&self, user: &U) -> Result<MapedRatings>;
-
-    fn user_by_name(&self, name: &str) -> Result<Vec<U>> {
-        Err(error::NotFoundByName(name.into()).into())
-    }
-
-    fn item_by_name(&self, name: &str) -> Result<Vec<I>> {
-        Err(error::NotFoundByName(name.into()).into())
-    }
+    fn users(&self, by: &SearchBy) -> Result<Vec<U>>;
+    fn items(&self, by: &SearchBy) -> Result<Vec<I>>;
+    fn ratings_by(&self, user: &U) -> Result<Ratings>;
+    fn ratings_except(&self, user: &U) -> Result<MapedRatings>;
 }
 
 pub mod error {
     use std::fmt::Debug;
+    use thiserror::Error as DError;
 
-    #[derive(Debug, thiserror::Error)]
-    #[error("Entity with name({0}) not found")]
-    pub struct NotFoundByName(pub String);
+    #[derive(Debug, Clone, DError)]
+    pub enum ErrorKind {
+        #[error("Couldn't found entity with id({0})")]
+        NotFoundById(String),
+
+        #[error("Couldn't found entity with name({0})")]
+        NotFoundByName(String),
+
+        #[error("Couldn't found entity with {0}({1})")]
+        NotFoundByCustom(String, String),
+    }
 }
