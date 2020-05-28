737efc489c827f7f84f12c729e9642d39011457c
Kevin Del Castillo <quebin31@gmail.com>
2020-05-28 12:10:57 -0500

    Implement Display on SearchBy

--- controller/src/lib.rs
+++ controller/src/lib.rs
@@ -1,6 +1,7 @@
 use anyhow::Error;
 use prettytable::{cell, format::consts::FORMAT_NO_LINESEP, row, table, Table};
 use std::collections::HashMap;
+use std::fmt::{self, Display};
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum SearchBy {
@@ -20,6 +21,16 @@ impl SearchBy {
 
     pub fn custom(key: &str, val: &str) -> Self {
         Self::Custom(key.into(), val.into())
+    }
+}
+
+impl Display for SearchBy {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SearchBy::Id(id) => write!(f, "id({})", id),
+            SearchBy::Name(name) => write!(f, "name({})", name),
+            SearchBy::Custom(key, val) => write!(f, "{}({})", key, val),
+        }
     }
 }
 
