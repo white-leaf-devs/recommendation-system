64f49ff0d8044c965d3876726ba77f37fbdec530
Kevin Del Castillo <quebin31@gmail.com>
2020-05-29 12:57:31 -0500

    Modified benches so one is shortest than the other

--- recommend/benches/distances.rs
+++ recommend/benches/distances.rs
@@ -27,11 +27,10 @@ fn generate_records(size: u64) -> (HashMap<u64, f64>, HashMap<u64, f64>) {
     let mut b = HashMap::new();
 
     for i in 0..size {
-        if rng.gen() {
-            a.insert(i, rng.gen_range(-10., 10.));
-        }
+        a.insert(i, rng.gen_range(-10., 10.));
 
-        if rng.gen() {
+        // Create one that's shortest
+        if i > (0.3 * size as f64) as u64 {
             b.insert(i, rng.gen_range(-10., 10.));
         }
     }
@@ -39,44 +38,49 @@ fn generate_records(size: u64) -> (HashMap<u64, f64>, HashMap<u64, f64>) {
     (a, b)
 }
 
-fn manhattan_distance_10000(c: &mut Criterion) {
-    let (a, b) = generate_records(10000);
+fn manhattan_distance_1000(c: &mut Criterion) {
+    let (a, b) = generate_records(1000);
 
-    c.bench_function("manhattan 10000 kinda", |bench| {
+    c.bench_function("manhattan 1000", |bench| {
         bench.iter(|| manhattan_distance(black_box(&a), black_box(&b)))
     });
 }
 
-fn manhattan_distance_100000(c: &mut Criterion) {
-    let (a, b) = generate_records(100_000);
+fn manhattan_distance_10_000(c: &mut Criterion) {
+    let (a, b) = generate_records(10_000);
 
-    c.bench_function("manhattan 100000 kinda", |bench| {
+    c.bench_function("manhattan 10000", |bench| {
         bench.iter(|| manhattan_distance(black_box(&a), black_box(&b)))
     });
 }
 
-fn euclidean_distance_10000(c: &mut Criterion) {
-    let (a, b) = generate_records(10000);
+fn euclidean_distance_1000(c: &mut Criterion) {
+    let (a, b) = generate_records(1000);
 
-    c.bench_function("euclidean 10000 kinda", |bench| {
+    c.bench_function("euclidean 1000", |bench| {
         bench.iter(|| euclidean_distance(black_box(&a), black_box(&b)))
     });
 }
 
-fn euclidean_distance_100000(c: &mut Criterion) {
-    let (a, b) = generate_records(100_000);
+fn euclidean_distance_10_000(c: &mut Criterion) {
+    let (a, b) = generate_records(10_000);
 
-    c.bench_function("euclidean 100000 kinda", |bench| {
+    c.bench_function("euclidean 10000", |bench| {
         bench.iter(|| euclidean_distance(black_box(&a), black_box(&b)))
     });
 }
 
-criterion_group!(
-    distances,
-    manhattan_distance_10000,
-    euclidean_distance_10000,
-    manhattan_distance_100000,
-    euclidean_distance_100000
-);
+criterion_group! {
+    name = distances_1000;
+    config = Criterion::default();
+    targets = manhattan_distance_1000, euclidean_distance_1000
+}
 
-criterion_main!(distances);
+criterion_group! {
+    name = distances_10_000;
+    config = Criterion::default();
+    targets = manhattan_distance_10_000, euclidean_distance_10_000
+
+}
+
+criterion_main!(distances_1000, distances_10_000);
