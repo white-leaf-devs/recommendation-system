6d522a4cede21e3fa41d47890787917293513e4b
Maria <mb.lev17@gmail.com>
2020-06-24 14:21:48 -0500

    Modify Shelves controller to use MongoDB

--- controllers/shelves/src/bin/load_means.rs
+++ controllers/shelves/src/bin/load_means.rs
@@ -38,7 +38,7 @@ fn main() -> Result<(), Error> {
     let url = &vars["DATABASE_URL"];
     let conn = establish_connection(url)?;
 
-    let controller = ShelvesController::with_url(url)?;
+    let controller = ShelvesController::with_url(url, "", "")?;
 
     let users_iterator = controller.users_by_chunks(10000);
     for user_chunk in users_iterator {
