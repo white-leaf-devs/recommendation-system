2c7db67166f4c6b949d47cf6f65c328223fdaa8c
Kevin Del Castillo <quebin31@gmail.com>
2020-06-25 20:25:48 -0500

    Add an extra field for means

--- controllers/movie-lens-small/migrations/2020-06-15-000503_create_means/up.sql
+++ controllers/movie-lens-small/migrations/2020-06-15-000503_create_means/up.sql
@@ -3,5 +3,6 @@ CREATE TABLE means
 (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id),
-    val FLOAT NOT NULL
+    val FLOAT NOT NULL,
+    score_number INTEGER NOT NULL
 )
