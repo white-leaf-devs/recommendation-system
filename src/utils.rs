60f333fab6e6a5aa2f4e6a4d9323d21db0234c4f
Kevin Del Castillo <quebin31@gmail.com>
2020-06-27 16:25:43 -0500

    Utilities to build prototypes

---
+++ src/utils.rs
@@ -0,0 +1,76 @@
+// Copyright (c) 2020 White Leaf
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+use anyhow::Error;
+use controller::{Field, Value};
+use rustyline::Editor;
+use std::collections::HashMap;
+
+macro_rules! field {
+    ($ed:ident, $name:expr, $opt:expr) => {{
+        use rustyline::error::ReadlineError;
+
+        let msg = if $opt {
+            format!("{}{} (optional): ", $crate::PROMPT, $name)
+        } else {
+            format!("{}{} (required): ", $crate::PROMPT, $name)
+        };
+
+        match $ed.readline(&msg) {
+            Ok(line) => Ok(Some(line)),
+
+            // CTRL-D
+            Err(ReadlineError::Eof) => Ok(None),
+
+            // Any error
+            Err(e) => Err(e),
+        }
+    }};
+}
+
+pub(crate) fn build_prototype<'a>(
+    rl: &mut Editor<()>,
+    fields: Vec<Field<'a>>,
+) -> Result<HashMap<&'a str, Value>, Error> {
+    println!("Press CTRL-D to leave a field as 'empty'");
+    let mut prototype = HashMap::new();
+
+    for field in fields {
+        let is_optional = field.is_optional();
+        let (name, ty) = field.into_tuple();
+
+        loop {
+            let input: Option<String> = field!(rl, name, is_optional)?;
+
+            match input {
+                Some(input) => {
+                    let value = Value::from_str(&input, ty);
+                    match value {
+                        Ok(value) => {
+                            prototype.insert(name, value);
+                            break;
+                        }
+
+                        Err(e) => {
+                            log::error!("Invalid value received!");
+                            log::error!("Reason: {}", e);
+                        }
+                    }
+                }
+
+                None if is_optional => {
+                    break;
+                }
+
+                None => {
+                    log::error!("Field '{}' is required, cannot be empty!", name);
+                    continue;
+                }
+            }
+        }
+    }
+
+    Ok(prototype)
+}
