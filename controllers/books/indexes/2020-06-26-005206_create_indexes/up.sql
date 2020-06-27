563909cdeacaf3e1254b58d3b93356302268500c
Kevin Del Castillo <quebin31@gmail.com>
2020-06-27 13:52:20 -0500

    Fixed index migration on books

--- controllers/books/indexes/2020-06-26-005206_create_indexes/up.sql
+++ controllers/books/indexes/2020-06-26-005206_create_indexes/up.sql
@@ -1,5 +1,5 @@
 -- Your SQL goes here
 
 CREATE INDEX ratings_user_id_idx on ratings(user_id);
-CREATE INDEX ratings_movie_id_idx on ratings(movie_id);
+CREATE INDEX ratings_movie_id_idx on ratings(book_id);
 CREATE INDEX means_user_id_idx on means(user_id);
