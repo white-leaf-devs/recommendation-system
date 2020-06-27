46f72184fedda0e4574e2bd486d75f788bd48d0f
Kevin Del Castillo <quebin31@gmail.com>
2020-06-27 16:24:20 -0500

    Parser for doubles

--- src/parser/basics.rs
+++ src/parser/basics.rs
@@ -1,12 +1,12 @@
 // Copyright (c) 2020 White Leaf
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
 use nom::bytes::complete::{tag, take_till1, take_while, take_while1};
 use nom::character::complete::{char, digit1};
 use nom::combinator::map_res;
-use nom::{sequence::delimited, IResult};
+use nom::{number::complete::double, sequence::delimited, IResult};
 
 pub(crate) fn parse_ident(input: &str) -> IResult<&str, &str> {
     take_while1(|c: char| c.is_alphanumeric() || c == '_' || c == '-')(input)
@@ -16,8 +16,12 @@ pub(crate) fn parse_string(input: &str) -> IResult<&str, &str> {
     delimited(char('\''), take_till1(|c: char| c == '\''), char('\''))(input)
 }
 
-pub(crate) fn parse_number(input: &str) -> IResult<&str, i64> {
+pub(crate) fn parse_int(input: &str) -> IResult<&str, i64> {
     map_res(digit1, |s: &str| s.parse::<i64>())(input)
+}
+
+pub(crate) fn parse_float(input: &str) -> IResult<&str, f64> {
+    double(input)
 }
 
 pub(crate) fn parse_separator(input: &str) -> IResult<&str, &str> {
@@ -60,12 +64,12 @@ mod tests {
 
     #[test]
     fn test_parse_numbers() {
-        let parsed = parse_number("12345");
+        let parsed = parse_int("12345");
         let expected = ("", 12345);
 
         assert_eq!(parsed, Ok(expected));
 
-        let parsed = parse_number("12c3");
+        let parsed = parse_int("12c3");
         let expected = ("c3", 12);
         assert_eq!(parsed, Ok(expected));
     }
