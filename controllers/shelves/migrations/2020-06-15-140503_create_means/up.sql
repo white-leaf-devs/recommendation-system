bf68c21e84194175ea5350e2ece21570e079f4a2
limonadev <limonadev@whiteleaf.dev>
2020-06-27 08:20:31 -0500

    Changed means migrations to use user_id as primary key

--- controllers/shelves/migrations/2020-06-15-140503_create_means/up.sql
+++ controllers/shelves/migrations/2020-06-15-140503_create_means/up.sql
@@ -1,7 +1,8 @@
 -- Your SQL goes here
 CREATE TABLE means
 (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id),
-    val FLOAT NOT NULL
+    user_id INTEGER REFERENCES users(id),
+    val FLOAT NOT NULL,
+    score_number INTEGER NOT NULL,
+    PRIMARY KEY (user_id)
 )
