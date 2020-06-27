3b79346c4509fb39803a01a28b42dd2151314d53
limonadev <limonadev@whiteleaf.dev>
2020-06-27 08:33:54 -0500

    Changed binary to load means with new table structure

--- controllers/shelves/src/bin/load_means.rs
+++ controllers/shelves/src/bin/load_means.rs
@@ -4,7 +4,7 @@
 // https://opensource.org/licenses/MIT
 
 use anyhow::Error;
-use controller::{Controller, Entity};
+use controller::Controller;
 use diesel::pg::PgConnection;
 use diesel::{insert_into, prelude::*};
 use shelves::establish_connection;
@@ -20,25 +20,27 @@ fn insert_means(conn: &PgConnection, new_means: &[NewMean]) -> Result<(), Error>
     Ok(())
 }
 
-fn compute_mean(ratings: &HashMap<i32, f64>) -> f64 {
+fn compute_mean(ratings: &HashMap<i32, f64>) -> Option<f64> {
     if ratings.is_empty() {
-        return 0.0;
+        return None;
     }
 
     let mut mean = 0.0;
     for rating in ratings.values() {
         mean += rating;
     }
-    mean / ratings.len() as f64
+    Some(mean / ratings.len() as f64)
 }
 
 fn main() -> Result<(), Error> {
     let vars: HashMap<String, String> = dotenv::vars().collect();
 
-    let url = &vars["DATABASE_URL"];
-    let conn = establish_connection(url)?;
+    let psql_url = &vars["DATABASE_URL"];
+    let mongo_url = &vars["MONGO_URL"];
+    let mongo_db = &vars["MONGO_DB"];
+    let conn = establish_connection(psql_url)?;
 
-    let controller = ShelvesController::with_url(url, "", "")?;
+    let controller = ShelvesController::with_url(psql_url, mongo_url, mongo_db)?;
 
     let users_iterator = controller.users_by_chunks(10000);
     for user_chunk in users_iterator {
@@ -46,15 +48,17 @@ fn main() -> Result<(), Error> {
         let now = Instant::now();
         let mut mean_chunk = Vec::new();
         let maped_ratings = controller.maped_ratings_by(&user_chunk)?;
-        for user in user_chunk {
-            let user_id = user.get_id();
-            if maped_ratings.contains_key(&user_id) {
-                let mean = compute_mean(&maped_ratings[&user_id]);
-                mean_chunk.push(NewMean { user_id, val: mean });
-            } else {
-                mean_chunk.push(NewMean { user_id, val: 0.0 });
+        for (user_id, ratings) in maped_ratings {
+            let mean = compute_mean(&ratings);
+            if let Some(mean) = mean {
+                mean_chunk.push(NewMean {
+                    user_id,
+                    val: mean,
+                    score_number: ratings.len() as i32,
+                });
             }
         }
+
         insert_means(&conn, &mean_chunk)?;
         println!("Elapsed per iteration: {}", now.elapsed().as_secs_f64());
     }
